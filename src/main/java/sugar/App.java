62622adad28a3744c08b7546245189f3cb2183ea
bbthekings <bbthekings@gmail.com>
2021-12-23 18:39:45 +0100

    added number output 1

--- src/main/java/sugar/App.java
+++ src/main/java/sugar/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( theSmaller(23, 230) );
+        System.out.println( theSmaller(123, 1230) );
     }
     
     public static int theSmaller(final int x, final int y) {
